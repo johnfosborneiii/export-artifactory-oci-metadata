a7c7d831fa047321a7f163b39043e6e44f087289
John Osborne <josborne@chainguard.dev>
2024-06-26 18:33:44 -0400

    Create fetch-oci-and-docker-repos.sh

---
+++ fetch-oci-and-docker-repos.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "token=$token"
+echo "artifactory_url=$artifactory_url"
+
+output_file=${1:-repositories.txt}
+
+# Step 1: Fetch repository names
+curl -G -H "Authorization: Bearer $token" "$artifactory_url/api/repositories/" | \
+jq '.[] | select(.packageType == "Docker" or .packageType == "OCI") | .key' > "$output_file"
+
+echo "Repository names fetched and saved to $output_file"
