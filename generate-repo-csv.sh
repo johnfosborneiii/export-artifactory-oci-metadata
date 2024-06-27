bf68cccbdd1392e422870f7bc8fdd46ebb9436e1
John Osborne <josborne@chainguard.dev>
2024-06-27 00:34:51 -0400

    Update generate-repo-csv.sh

--- generate-repo-csv.sh
+++ generate-repo-csv.sh
@@ -14,22 +14,28 @@ else
 fi
 
 csvimageslistfile=$(mktemp --suffix=.csv)
-echo "repo,path,sha256,created,modified,updated,stat_downloaded,properties" > $csvimageslistfile
-
-echo ""
-echo "results will be saved to: $csvimageslistfile"
-echo ""
+echo "repo,path,sha256,created,modified,updated,properties,downloaded,downloads" > "$csvimageslistfile"
 
 while IFS= read -r repo; do
+    
     echo "Querying repository: $repo"
-    output=$(curl -s -X POST -H "Authorization: Bearer $token" -H "Content-Type: text/plain" "$artifactory_url/api/search/aql" --data "items.find({\"repo\":{\"\$match\":\"$repo\"}}).include(\"repo\",\"path\",\"sha256\",\"created\",\"modified\",\"updated\",\"stat.downloaded\",\"property.*\")")
-    json_output=$(echo "$output" | jq -c '.results[] | select(.properties != null and (.properties[] | select(.key == "docker.manifest.type" and .value == "application/vnd.oci.image.index.v1+json"))) | 
-    {repo, path, sha256, created, modified, updated, stat_downloaded, properties: [.properties[] | select(.key == "docker.manifest") | {(.key): .value}]}')
+    echo ""
 
-    # Force remove the double quotes because jq --raw-output does not work when piping to CSV to remove double quotes
-    echo "$json_output" | jq -r '[.repo, .path, .sha256, .created, .modified, .updated, .stat_downloaded, (.properties | map(.["docker.manifest"]) | join(";"))] | @csv' | awk '{gsub(/"/,"")};1' >> $csvimageslistfile
+    output=$(curl -s -X POST -H "Authorization: Bearer $token" -H "Content-Type: text/plain" "$artifactory_url/api/search/aql" --data "items.find({\"repo\":{\"\$match\":\"$repo\"}}).include(\"repo\",\"path\",\"sha256\",\"created\",\"modified\",\"updated\",\"property.*\",\"stat.downloaded\",\"stat.downloads\")")
+    json_output=$(echo "$output" | jq -c '.results[] | select(.properties != null and (.properties[] | select(.key == "docker.manifest.type" and .value == "application/vnd.oci.image.index.v1+json"))) | 
+    {repo, path, sha256, created, modified, updated, properties: (.properties | map(.value) | join(";")), downloaded: .["stats"][0]["downloads"], downloads: .["stats"][0]["downloaded"]}')
+    
+    # Convert to CSV format and remove double quotes
+    echo "$json_output" | jq -r '[.repo, .path, .sha256, .created, .modified, .updated, .properties, .downloads, .downloaded] | @csv' | sed 's/"//g' >> "$csvimageslistfile"
     
 done < "$repositories_file"
 
 echo "---------------------------------------------"
+echo ""
+cat "$csvimageslistfile"
+echo ""
+echo "---------------------------------------------"
+echo ""
 echo "CSV file with image data saved to: $csvimageslistfile"
+echo ""
+echo "---------------------------------------------"
