666c7264aaaa7e6087e6ce4e79035d76dcdc11bb
John Osborne <josborne@chainguard.dev>
2024-06-26 22:58:57 -0400

    Update fetch-oci-and-docker-repos.sh

--- fetch-oci-and-docker-repos.sh
+++ fetch-oci-and-docker-repos.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo ""
-echo "artifactory_url=$artifactory_url"
-echo ""
 
 output_file=${1:-repositories.txt}
 
 # Step 1: Fetch repository names
-curl -G -H "Authorization: Bearer $token" "$artifactory_url/api/repositories/" | \
-jq '.[] | select(.packageType == "Docker" or .packageType == "OCI") | .key' > "$output_file"
+reponames=$(curl -s -X POST -H "Authorization: Bearer $token" -H "Content-Type: text/plain" "https://cgrdemo.jfrog.io/artifactory/api/search/aql" --data 'items.find().include("repo")' | jq -r '.results[].repo')
 
-echo "Repository names fetched and saved to $output_file"
+echo ""
+echo "The following repo names have been save to $output_file:"
+echo ""
+echo "$reponames" > $output_file
+echo "$reponames"
